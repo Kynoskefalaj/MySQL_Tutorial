fa464142cf5fc2d18e57f5a10a8d9af97777b107
Adam Gumiński <adam.guminski@gmail.com>
2023-04-04 00:13:20 +0200

    First exercise

---
+++ Exercises.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE animals;
+SHOW DATABASES;
+-- usuń bazę animals 
+DROP DATABASE animals;
+-- wejdz do bazy informations_schema
+SHOW DATABASES;
+USE information_schema;
+-- wylistuj dostępne tabele
+SELECT database();
+-- wyswietl strukture tabeli zawierajacej jednostki miar
+SHOW TABLES;
+DESC ST_UNITS_OF_MEASURE;
+-- stworz nowa tabele fastest_animals
+CREATE DATABASE fastest_animals;
+SHOW DATABASES;
