4bad5884b58200db0cf85dcd435441dfffb28020
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-04-05 00:33:05 +0200

    exercises 5 and 6 resolved

---
+++ Ex_5-6.sql
@@ -0,0 +1,26 @@
+-- CREATE TABLE slowest_animals (
+-- 				animal CHAR(20),
+--                 species CHAR(20),
+--                 speed FLOAT(10,6));
+
+-- INSERT INTO slowest_animals
+-- 				(animal, species, speed) VALUES
+-- 				('American Woodcock', 'bird', 8.0);
+--             
+-- INSERT INTO slowest_animals
+-- 				(animal, species, speed) VALUES
+-- 				('Giant Tortoise', 'reptile', 1.6);
+--             
+-- INSERT INTO slowest_animals
+-- 				(species, animal, speed) VALUES
+-- 				('fish', 'Seahorse', 0.0161);
+--             
+-- INSERT INTO slowest_animals
+-- 				(animal, speed) VALUES
+-- 				('Banana Snail', 0.000083);
+--             
+-- INSERT INTO slowest_animals
+-- 				(animal, species) VALUES
+--                 ('Reef', 'coral');
+
+SELECT * FROM slowest_animals;
