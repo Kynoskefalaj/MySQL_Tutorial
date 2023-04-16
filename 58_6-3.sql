730d9035be5fe642d2788f3685663cfc0bc5da9a
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-04-16 13:19:25 +0200

    58_6-3

--- 58_6-3.sql
+++ 58_6-3.sql
@@ -1,2 +1,40 @@
 USE books;
 
+SELECT
+	title,
+    rating,
+    CASE
+		WHEN rating > 4 THEN 'good book'
+        ELSE 'good enough book'
+	END AS type
+FROM books;
+
+SELECT
+	title,
+    rating,
+    CASE
+		WHEN rating > 4 THEN 'good book'
+        ELSE 'good enough book'
+	END AS type,
+    author
+FROM books;
+
+
+SELECT
+	title,
+    rating,
+    CASE
+		WHEN rating > 4.2 THEN 'very good book'
+        WHEN rating BETWEEN 3.8 AND 4.2 THEN 'good book'
+        ELSE 'not that good book'
+	END AS type,
+    author
+FROM books;
+
+
+SELECT
+	title,
+    rating,
+    IF(rating > 4, 'good book', 'not that good book'),
+    author
+FROM books;
